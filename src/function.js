1fef635cc89f19e0f0cf084fbaf6e88d8472bb61
Derrick wakhu shibero <shiberoderrickwakhu@gmail.com>
2024-11-01 12:44:05 +0300

    removed code related to btn

--- src/function.js
+++ src/function.js
@@ -1,5 +1,5 @@
 // buttons elements
-var btn = document.querySelector("#btn");
+// const btn = document.querySelector<HTMLButtonElement>("#btn")
 var name_btn = document.querySelector("#name_btn");
 var level_btns = [
     document.querySelector("#very_good"),
@@ -67,45 +67,8 @@ var get_name = function () {
     name_div.style.display = "none";
     question_div.style.display = "block";
     question_div.style.visibility = "visible";
-};
-// main function for the btn in the questions div
-var get_value = function () {
-    if (index % 2 == 0) {
-        // gives the asks user to state their name
-        name_question();
-        index += 1;
-    }
-    else {
-        // function that deals with questions
-        choose_function();
-    }
-    // responsible for changing the background color
+    // for changing the background color
     color_change();
-};
-var name_question = function () {
-    beginner.innerHTML = "What is your name?";
-    compliment.innerHTML = "Hello ".concat(entry.value);
-};
-var choose_function = function () {
-    if (choose < questions.length) {
-        // generates the questions
-        question_reader();
-        choose += 1;
-    }
-    else {
-        // returns the career
-        read_career();
-        index += 1;
-        choose = 0;
-    }
-};
-var question_reader = function () {
-    beginner.innerHTML = questions[choose];
-    compliment.innerHTML = "Click the button to continue";
-};
-var read_career = function () {
-    beginner.innerHTML = "Your career is ";
-    compliment.innerHTML = careers[Math.floor(Math.random() * careers.length)];
 };
 var color_change = function () {
     var r = Math.floor(Math.random() * 255);
@@ -115,5 +78,4 @@ var color_change = function () {
     body.style.background = "rgb(".concat(color, ")");
 };
 // adding event listeners
-btn.addEventListener("click", get_value);
 name_btn.addEventListener("click", get_name);
