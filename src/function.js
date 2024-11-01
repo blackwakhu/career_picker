86442e86a0e85e5c3d9135a87bcc9024700641ec
Derrick wakhu shibero <shiberoderrickwakhu@gmail.com>
2024-11-01 09:43:00 +0300

    added all the required variables

--- src/function.js
+++ src/function.js
@@ -1,2 +1,6 @@
 var btn = document.querySelector("#btn");
 var body = document.querySelector("body");
+var questions = ["are you creative?", "do you love nature?", "are you into helping others?", "do you love to build new things?", "do you consider yourself to be compassionate?"];
+var index = 0;
+var careers = ["doctor", "computer scientist", "teacher", "engineer", "scientist", "surgeon", "pilot", "enviromentalist", "model", "astronaught"];
+var choose = 0;
--- src/function.ts
+++ src/function.ts
@@ -1,3 +1,7 @@
-let btn: HTMLDivElement = document.querySelector("#btn")
+let btn: HTMLDivElement | null = document.querySelector("#btn")
+let body: HTMLBodyElement | null = document.querySelector("body")
 
-let body: HTMLBodyElement = document.querySelector("body")
+let questions = ["are you creative?", "do you love nature?", "are you into helping others?", "do you love to build new things?", "do you consider yourself to be compassionate?"]
+let index: number = 0
+let careers = ["doctor", "computer scientist", "teacher","engineer", "scientist","surgeon","pilot","enviromentalist","model","astronaught"]
+let choose: number = 0
