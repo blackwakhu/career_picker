5baa85d59853b1bf776eb705a91de6956e144b22
Derrick wakhu shibero <shiberoderrickwakhu@gmail.com>
2024-11-01 10:04:06 +0300

    finished name_question

--- src/function.js
+++ src/function.js
@@ -2,12 +2,24 @@ var btn = document.querySelector("#btn");
 var body = document.querySelector("body");
 var beginner = document.querySelector("#beginner");
 var compliment = document.querySelector("#compliment");
+var entry = document.querySelector("#entry");
 var questions = ["are you creative?", "do you love nature?", "are you into helping others?", "do you love to build new things?", "do you consider yourself to be compassionate?"];
 var index = 0;
 var careers = ["doctor", "computer scientist", "teacher", "engineer", "scientist", "surgeon", "pilot", "enviromentalist", "model", "astronaught"];
 var choose = 0;
+// main function
 var get_value = function () {
+    if (index % 2 == 0) {
+        // gives the asks user to state their name
+        name_question();
+        index += 1;
+    }
+    // responsible for changing the background color
     color_change();
+};
+var name_question = function () {
+    beginner.innerHTML = "What is your name?";
+    compliment.innerHTML = "Hello ".concat(entry.value);
 };
 var color_change = function () {
     var r = Math.floor(Math.random() * 255);
--- src/function.ts
+++ src/function.ts
@@ -2,6 +2,7 @@ const btn: HTMLDivElement = document.querySelector("#btn")
 const body: HTMLBodyElement  = document.querySelector("body")
 const beginner: HTMLDivElement = document.querySelector("#beginner")
 const compliment: HTMLDivElement = document.querySelector("#compliment")
+const entry: HTMLInputElement = document.querySelector("#entry")
 
 let questions = ["are you creative?", "do you love nature?", "are you into helping others?", "do you love to build new things?", "do you consider yourself to be compassionate?"]
 let index: number = 0
@@ -9,8 +10,20 @@ let careers = ["doctor", "computer scientist", "teacher","engineer", "scientist"
 let choose: number = 0
 
 
+// main function
 let get_value = function ()  {
+    if (index % 2 == 0)  {
+        // gives the asks user to state their name
+        name_question()
+        index += 1
+    }
+    // responsible for changing the background color
     color_change()
+}
+
+let name_question = function ()  {
+    beginner.innerHTML = "What is your name?"
+    compliment.innerHTML = `Hello ${entry.value}`
 }
 
 let color_change = function () {
