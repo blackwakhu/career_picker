e4d30710c3afd93f70f5c77091228383fc7828b5
Derrick wakhu shibero <shiberoderrickwakhu@gmail.com>
2024-11-01 10:11:41 +0300

    added the choose_function() question_reader, read_career

--- src/function.js
+++ src/function.js
@@ -4,8 +4,8 @@ var beginner = document.querySelector("#beginner");
 var compliment = document.querySelector("#compliment");
 var entry = document.querySelector("#entry");
 var questions = ["are you creative?", "do you love nature?", "are you into helping others?", "do you love to build new things?", "do you consider yourself to be compassionate?"];
-var index = 0;
 var careers = ["doctor", "computer scientist", "teacher", "engineer", "scientist", "surgeon", "pilot", "enviromentalist", "model", "astronaught"];
+var index = 0;
 var choose = 0;
 // main function
 var get_value = function () {
@@ -14,6 +14,10 @@ var get_value = function () {
         name_question();
         index += 1;
     }
+    else {
+        // function that deals with questions
+        choose_function(index, choose);
+    }
     // responsible for changing the background color
     color_change();
 };
@@ -21,6 +25,19 @@ var name_question = function () {
     beginner.innerHTML = "What is your name?";
     compliment.innerHTML = "Hello ".concat(entry.value);
 };
+var choose_function = function (index, choose) {
+    if (choose < questions.length) {
+        question_reader();
+        choose += 1;
+    }
+    else {
+        read_career();
+        index += 1;
+        choose = 0;
+    }
+};
+var question_reader = function () { };
+var read_career = function () { };
 var color_change = function () {
     var r = Math.floor(Math.random() * 255);
     var g = Math.floor(Math.random() * 255);
--- src/function.ts
+++ src/function.ts
@@ -4,9 +4,10 @@ const beginner: HTMLDivElement = document.querySelector("#beginner")
 const compliment: HTMLDivElement = document.querySelector("#compliment")
 const entry: HTMLInputElement = document.querySelector("#entry")
 
-let questions = ["are you creative?", "do you love nature?", "are you into helping others?", "do you love to build new things?", "do you consider yourself to be compassionate?"]
+const questions = ["are you creative?", "do you love nature?", "are you into helping others?", "do you love to build new things?", "do you consider yourself to be compassionate?"]
+const careers = ["doctor", "computer scientist", "teacher","engineer", "scientist","surgeon","pilot","enviromentalist","model","astronaught"]
+
 let index: number = 0
-let careers = ["doctor", "computer scientist", "teacher","engineer", "scientist","surgeon","pilot","enviromentalist","model","astronaught"]
 let choose: number = 0
 
 
@@ -16,6 +17,9 @@ let get_value = function ()  {
         // gives the asks user to state their name
         name_question()
         index += 1
+    } else  {
+        // function that deals with questions
+        choose_function(index, choose)
     }
     // responsible for changing the background color
     color_change()
@@ -25,6 +29,21 @@ let name_question = function ()  {
     beginner.innerHTML = "What is your name?"
     compliment.innerHTML = `Hello ${entry.value}`
 }
+
+let choose_function = function (index, choose)  {
+    if (choose < questions.length)  {
+        question_reader()
+        choose += 1
+    } else  {
+        read_career()
+        index += 1 
+        choose = 0
+    }
+}
+
+let question_reader = function () {}
+
+let read_career = function ()  {}
 
 let color_change = function () {
     let r: number = Math.floor(Math.random() * 255)
