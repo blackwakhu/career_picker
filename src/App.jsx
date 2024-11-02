42c1c12a5173c2f112d39b5b89a88caa207ab8a6
Derrick wakhu shibero <shiberoderrickwakhu@gmail.com>
2024-11-02 13:58:19 +0300

    added namestr

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,19 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [namestr, setNamestr] = useState("What is your name?")
 
   return (
     <>
-      
+      <div>
+        <h1>Career Questionnaire</h1>
+      </div>
+      <div>
+        <h2>{namestr}</h2>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
     </>
   )
 }
